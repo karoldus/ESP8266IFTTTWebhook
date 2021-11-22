bc68e68241c5d123e59b27e0137ec2be83c599e0
Wirgiliusz <kamx98@gmail.com>
2021-11-22 15:16:42 +0100

    Fix error with missing client argument for http.begin()
    New HTTPClient API requires additional WiFiClient object as an argument to begin() function.

--- ESP8266IFTTTWebhook.cpp
+++ ESP8266IFTTTWebhook.cpp
@@ -2,10 +2,11 @@
 #include "Arduino.h"
 #include "ESP8266IFTTTWebhook.h"
 
-ESP8266IFTTTWebhook::ESP8266IFTTTWebhook (String EVENT_NAME, String API_KEY)
+ESP8266IFTTTWebhook::ESP8266IFTTTWebhook (String EVENT_NAME, String API_KEY, WiFiClient CLIENT)
 {
 	_EVENT_NAME = EVENT_NAME;
 	_API_KEY = API_KEY;
+    _CLIENT = CLIENT;
 }
 
 
@@ -30,7 +31,7 @@ void ESP8266IFTTTWebhook::trigger(String value1, String value2, String value3)
 {
 	HTTPClient http;
 
-    http.begin("http://maker.ifttt.com/trigger/" + _EVENT_NAME + "/with/key/" + _API_KEY);
+    http.begin(_CLIENT, "http://maker.ifttt.com/trigger/" + _EVENT_NAME + "/with/key/" + _API_KEY);
     http.addHeader("Content-Type", "application/json");
 	
 	String values = "{\"value1\":\"" + value1 + "\", \"value2\":\"" + value2 + "\", \"value3\":\"" + value3 + "\"}";
